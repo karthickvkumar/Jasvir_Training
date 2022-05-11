ae45d89f7cadc04425990ce1fa7a501453e9600a
kvelraj_concord <kvelraj@concord.net>
2022-05-11 09:31:28 +0530

    data types

---
+++ HTML Tutorial/program.js
@@ -0,0 +1,53 @@
+// Number -> Any numerical value from Positive, Negative, Fractional, Decimal
+
+// Syntax - To create a Variable (Temp Storage)
+// var variable_name = value;
+
+// variable_name -> Any valid text, it should contains a-z, A-Z, 0-9, _, $
+
+var age = 28;
+console.log(age);
+
+// String -> Any text value which is typed inside '' or ""
+
+var email_id = "karthick@gmail.com";
+console.log(email_id);
+
+// Boolean -> Either true or false
+
+var result = true;
+console.log(result);
+
+console.log(null);
+
+var future_value;
+console.log(future_value);
+
+email_id = "abc@testdemo.com";
+console.log(email_id);
+
+// Object -> It is collection of Key and Values
+
+// Syntax of Object
+
+// var variable_name = {
+//   key1 : value,
+//   key2 : value
+// }
+
+// key -> Similar to variable_name -> it should contains a-z, A-Z, 0-9, _, $
+// value -> Number, String, Boolean, Object, Array, Function
+
+var first_name = "karthick";
+var last_name = "kumar";
+var roll_number = 4785588;
+var area = "chennai";
+
+var studentInfo = {
+  first_name : "karthick",
+  last_name : "kumar",
+  roll_number : 4785588,
+  location : "chennai"
+};
+
+console.log(studentInfo)
