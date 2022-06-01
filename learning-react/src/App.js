aa31879a11c800005e03debc84e27cb87d8c40ff
kvelraj_concord <kvelraj@concord.net>
2022-06-01 09:10:22 +0530

    class component

--- learning-react/src/App.js
+++ learning-react/src/App.js
@@ -1,8 +1,10 @@
+import HeaderComponent from "./components/header";
 
 function App() {
   return (
     <div>
       <h1>Welocome to React JS</h1>
+      <HeaderComponent></HeaderComponent>
     </div>
   );
 }
