7781d11a70aa050f13110f2203f1e194e5122af0
kvelraj_concord <kvelraj@concord.net>
2022-06-03 09:17:11 +0530

    react event

--- learning-react/src/components/login-action.js
+++ learning-react/src/components/login-action.js
@@ -10,19 +10,23 @@ class LoginActionComponent extends Component{
     alert("Form is submitted");
   }
 
+  handleInput(event){
+    console.log(event.target.value);
+  }
+
   render(){
     return(
       <div>
         <div>
           <label className="label">Enter your Email Id</label>
-          <input type="text" className="inputbox" placeholder="Enter your email id"/>
+          <input type="text" className="inputbox" placeholder="Enter your email id" onChange={ this.handleInput.bind(this) }/>
         </div>
         <div className="space">
           <label className="label">Enter your Password</label>
-          <input type="password" className="inputbox" placeholder="Enter your password" />
+          <input type="password" className="inputbox" placeholder="Enter your password" onChange={ this.handleInput.bind(this) }/>
         </div>
         <div className="space">
-          <button onClick={() => this.loginSubmit() }>Login</button>
+          <button onClick={() => this.loginSubmit()}>Login</button>
         </div>
       </div>
     )
