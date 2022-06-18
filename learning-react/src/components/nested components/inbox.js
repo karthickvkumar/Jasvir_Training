6a631f963a4cc3def9e8b45dcea66b46379722f2
kvelraj_concord <kvelraj@concord.net>
2022-06-18 09:09:33 +0530

    api

--- learning-react/src/components/nested components/inbox.js
+++ learning-react/src/components/nested components/inbox.js
@@ -1,9 +1,24 @@
 import React from "react";
+import axios from "axios";
 
 const InboxComponent = () => {
+
+  const loadUsers = () => {
+    const url = "https://reqres.in/api/users?page=2";
+
+    axios.get(url)
+        .then((success) => {
+          console.log(success.data);
+        })
+        .catch((failure) => {
+          console.log(failure);
+        });
+  }
+
   return(
     <div>
       <h1>This is a Inbox components</h1>
+      <button onClick={() => loadUsers()}>Load List of Users</button>
     </div>
   )
 }
