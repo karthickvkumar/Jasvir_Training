89e0a9d8cb603f6ce452c94787b9f7f03df25faa
kvelraj_concord <kvelraj@concord.net>
2022-06-22 09:07:29 +0530

    get api

--- learning-react/src/components/nested components/inbox.js
+++ learning-react/src/components/nested components/inbox.js
@@ -1,24 +1,55 @@
-import React from "react";
+import React, {useState} from "react";
 import axios from "axios";
 
 const InboxComponent = () => {
+
+  const [userList, setUserList] = useState([]);
 
   const loadUsers = () => {
     const url = "https://reqres.in/api/users?page=2";
 
     axios.get(url)
         .then((success) => {
-          console.log(success.data);
+          const response = success.data;
+          setUserList(response.data);
         })
         .catch((failure) => {
           console.log(failure);
         });
   }
 
+  const profiles = userList.map((value, index) => {
+    return(
+      <tr key={index}>
+        <td>{value.id}</td>
+        <td>{value.first_name}</td>
+        <td>{value.last_name}</td>
+        <td>{value.email}</td>
+        <td>
+          <img src={value.avatar} />
+        </td>
+      </tr>
+    )
+  })
+
   return(
     <div>
       <h1>This is a Inbox components</h1>
       <button onClick={() => loadUsers()}>Load List of Users</button>
+      <table id="customers">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email ID</th>
+            <th>Avatar</th>
+          </tr>
+        </thead>
+        <tbody>
+          {profiles}
+        </tbody>
+      </table>
     </div>
   )
 }
